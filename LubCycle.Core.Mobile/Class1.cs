277f5517608c723c2375e67372e51b997d6dbddb
Mateusz Dudek <mateudu@outlook.com>
2016-07-02 07:54:33 +0200

    Started LubCycle.Android and LubCycle.Core.Mobile project, added references to LubCycle.Core.Mobile.

---
+++ LubCycle.Core.Mobile/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LubCycle.Core.Mobile
+{
+    public class Class1
+    {
+        public string Hello()
+        {
+            return "Hello world!";
+        }
+    }
+}
